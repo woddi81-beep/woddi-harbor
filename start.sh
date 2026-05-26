e12efea0c3f09da3be1212f864556e16d0c14829
woddi81-beep <woddi81@gmail.com>
2026-05-26 10:24:06 +0200

    Add start.sh with logging support

---
+++ start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd /srv/http/woddi-harbor
+LOGDIR="$HOME/.harbor/logs"
+mkdir -p "$LOGDIR"
+LOGFILE="$LOGDIR/harbor.log"
+exec .venv/bin/uvicorn app.control:create_app --factory --host 127.0.0.1 --port 9680 --access-log 2>> "$LOGFILE"
